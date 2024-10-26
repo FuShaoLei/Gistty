e3d8b36282738b0a8f7540956f6cca4c4db315a5
fushaolei <1563250958@qq.com>
2024-10-26 11:53:11 +0800

    style 优化

--- src/components/GistTest.vue
+++ src/components/GistTest.vue
@@ -34,7 +34,7 @@
         <div class="tag_line"></div>
         <div
             :class="['tag_button standard_button', (topShowData.type === 'tag' && topShowData.name === key) ? 'active' : '']"
-             v-for="key in tagDataArr.keys()" @click="handleClickTag(key)">
+             v-for="key in tagMap.keys()" @click="handleClickTag(key)">
           <i class="ri-hashtag"></i>
           {{ key }}
         </div>
@@ -74,7 +74,7 @@
       <div class="detail_content">
         <div v-for="file in currentClickItem.files" class="detail_item">
           <div class="detail_item_file"> {{ file.filename }} </div>
-          <div class="detail_item_content"> <pre>{{file.rawContent}}</pre></div>
+          <div class="detail_item_content"><pre>{{file.rawContent}}</pre></div>
         </div>
       </div>
     </div>
@@ -121,78 +121,66 @@ const { proxy } =  getCurrentInstance()
 
 const gistAddOrUpdateDialogRef = ref(null)
 
-const gistDataArr = ref([]) // 我的全部数据
+const allGistData = ref([]) // 我的全部数据
+const allStarGistData = ref([])
+const showGistData = ref([]) // 实际展示的数据
 
-const showGistData = ref([]) // 实际展示的数据 有可能是 我的， 也可能是star的
 
-const tagDataArr = ref(new Map())
+const tagMap = ref(new Map())
 
 const handleTagData = (arr, id) => {
   arr.forEach((item) => {
-    if (!tagDataArr.value.has(item)) {
+    if (!tagMap.value.has(item)) {
       let newSet = new Set()
       newSet.add(id)
-      tagDataArr.value.set(item, newSet);
+      tagMap.value.set(item, newSet);
     } else {
-      let currentSet = tagDataArr.value.get(item)
+      let currentSet = tagMap.value.get(item)
       currentSet.add(id)
-      tagDataArr.value.set(item, currentSet);
+      tagMap.value.set(item, currentSet);
     }
   })
 }
 
+
+
 const getGistArr = async () => {
   try {
     const res = await getGist();
-    const allData = res.data;
+    const newAllData = res.data;
 
-    // 使用 Promise.all 等待所有异步操作完成
-    const updatedDataPromises = allData.map(async (item) => {
-      const filePromises = Object.values(item.files).map(async (objValue) => {
-        const rawContent = await getContent(objValue.raw_url);
-        return Object.assign(objValue, {rawContent});
-      });
+    // const updatedDataPromises = allData.map(async (item) => {
+    //   const filePromises = Object.values(item.files).map(async (objValue) => {
+    //     const rawContent = await getContent(objValue.raw_url);
+    //     return Object.assign(objValue, {rawContent});
+    //   });
+    //
+    //   // 等待所有文件的内容都获取完
+    //   await Promise.all(filePromises);
+    //   return item;
+    // });
+    //
+    // // 等待所有数据的更新操作完成
+    // const updatedData = await Promise.all(updatedDataPromises);
 
-      // 等待所有文件的内容都获取完
-      await Promise.all(filePromises);
-      return item;
-    });
-
-    // 等待所有数据的更新操作完成
-    const updatedData = await Promise.all(updatedDataPromises);
-
-    updatedData.forEach(ele => {
-      console.log("ele.description = " + ele.description)
-      // let tags = ele.description.match(/#(\w+)/g).map(tag => tag.slice(1));
-      // const text = "这里是一些文字，其中包含 #life #coding #生命 #生活";
-
+    newAllData.forEach(ele => {
       if (ele.description !== undefined && ele.description !== null) {
-
         const matchData = ele.description.match(/#([\w\u4e00-\u9fa5]+)/g);
-
         if(matchData !== null) {
           let tags = matchData.map(tag => tag.slice(1));
-
           if (tags.length > 0) {
-            console.log(tags); // 输出: ["life", "coding"]
             handleTagData(tags, ele.id)
           }
         }
       }
     })
 
-    console.log("tagDataArr.value")
-    console.log(tagDataArr.value)
-
-    gistDataArr.value = updatedData;
-
+    allGistData.value = [...newAllData]
     // TODO 可能要处理一些东西
-
-    showGistData.value = [...gistDataArr.value]
-
+    showGistData.value = [...allGistData.value]
 
     if (currentClickItem.value.id !== undefined) {
-      currentClickItem.value = gistDataArr.value.find(ele => ele.id === currentClickItem.value.id)
+      currentClickItem.value = allGistData.value.find(ele => ele.id === currentClickItem.value.id)
     }
 
   } catch (error) {
@@ -200,8 +188,19 @@ const getGistArr = async () => {
   }
 };
 
+const handleGetRawContentData = async (item) => {
 
-const test_url = "https://gist.githubusercontent.com/FuShaoLei/3bc755decb52a8d81f08ed48d870cd82/raw/e0ca010f35a59c568b15b3943d35f79271eae456/activity.kt";
+    const filePromises = Object.values(item.files).map(async (objValue) => {
+      const rawContent = await getContent(objValue.raw_url);
+      return Object.assign(objValue, {rawContent});
+    });
+
+    // 等待所有文件的内容都获取完
+    await Promise.all(filePromises);
+
+    return item;
+}
+
 
 const getContent = async (url) => {
   let result = null
@@ -216,20 +215,13 @@ const getContent = async (url) => {
   return result
 };
 
-const testGetContent = async () => {
-  const result = await getContent(test_url);
-  console.log("testGetContent")
-  console.log(result)
-}
-
-// testGetContent()
-
 const currentClickItem = ref({})
 
-const handleClickLeftItem = (data) => {
+const handleClickLeftItem = async (data) => {
   console.log("handleClickLeftItem")
   console.log(data)
   currentClickItem.value = data
+  currentClickItem.value = await handleGetRawContentData(data)
 
 }
 
@@ -302,29 +294,16 @@ const openLoginDialog = () => {
 }
 
 const confirmLoginDialog = (data) => {
-  // TODO 存储
-  console.log("confirmLoginDialog")
-  console.log(data)
-
   mSettingsStore.setLoginSettings({token: data})
-
-  console.log("after token")
-
-  console.log(mSettingsStore.loginData.token)
-
   closeLoginDialog()
-
   init()
-
 }
 
 const mSettingsStore = useSettingsStore()
 const init = () => {
-  // TODO 取出存储 看看是否有token
   if (mSettingsStore.loginData.token.length === 0) {
-    // TODO 清除当前数据
     currentClickItem.value = {}
-    gistDataArr.value = []
+    allGistData.value = []
     openLoginDialog()
   } else {
     getGistArr()
@@ -380,9 +359,9 @@ const handleClickTag = (key) => {
   // TODO 处理数据的展示
 
   // TODO 名称后面改成tagMap
-  const idSet = tagDataArr.value.get(key)
+  const idSet = tagMap.value.get(key)
 
-  showGistData.value = [...gistDataArr.value].filter(ele => idSet.has(ele.id))
+  showGistData.value = [...allGistData.value].filter(ele => idSet.has(ele.id))
 
   currentClickItem.value = {}
 }
@@ -395,7 +374,7 @@ const handleClickType = (key) => {
     // TODO 请求我的自己全部gist数据
 
     // TODO 这里应该重新请求一次，为了好看先用原来的数据
-    showGistData.value = [...gistDataArr.value]
+    showGistData.value = [...allGistData.value]
   }
 }
 
